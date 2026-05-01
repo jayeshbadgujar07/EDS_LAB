e2d0ea2733f125db04b710acd4235d3eeac2b96a
Jayesh Ramchandra Badgujar <jayeshbadgujar201007@gmail.com>
2026-05-01 17:03:49 +0530

    Add files via upload

---
+++ PRACTICAL 2/list.py
@@ -0,0 +1,16 @@
+num = [10,20,30,40]
+print(num)
+
+print(num[0])
+print(num[1])
+
+
+list1=[1,2]
+print(list1*3)
+list2=[3,4]
+print(list1 + list2)
+
+a=[1,2,3]
+a.append(7)
+print(a)
+
---
+++ PRACTICAL 2/marks.py
@@ -0,0 +1,9 @@
+marks=[75,82,90,68,88]
+print("marks",marks)
+marks.append(95)
+print("new marks",marks)
+
+print(marks[0])
+marks.sort()
+print(marks)
+print("max marks:",max(marks))
